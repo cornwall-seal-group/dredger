f32c4b9efaf958c43ce0ad51d3e5aee1515aa998
Rich Frost <richard@ipponsolutions.co.uk>
2019-09-16 07:20:25 +0100

    Updated with cc rotations

--- train/trainer.py
+++ train/trainer.py
@@ -3,6 +3,8 @@ from azure.cognitiveservices.vision.customvision.training.models import ImageFil
 import os
 import config
 from PIL import Image
+from math import ceil
+import io
 
 IMAGES_FOLDER = config.IMAGES_FOLDER
 ENDPOINT = config.ENDPOINT
@@ -19,7 +21,7 @@ def create_classifier_model(tag):
 
     tags = {}
     image_list = []
-    required_image_count = 4
+    required_image_count = 10
 
     for seal_name in os.listdir(IMAGES_FOLDER):
 
@@ -27,39 +29,59 @@ def create_classifier_model(tag):
 
         if seal_name == 'pjf348':
 
-            tag_path = os.path.join(seal_name, tag)
+            tag_path = os.path.join(IMAGES_FOLDER, seal_name, tag)
 
             if os.path.isdir(tag_path):
                 tags[seal_name] = trainer.create_tag(project.id, seal_name)
 
-                num_images_for_seal = len(
-                    [name for name in os.listdir(tag_path) if os.path.isfile(name)])
+                tagpath, tagdirs, tagfiles = next(os.walk(tag_path))
+                num_images_for_seal = len(tagfiles)
+
                 for file in os.listdir(tag_path):
                     num_rotations_per_image = int(
                         ceil(required_image_count / num_images_for_seal))
 
-                    rotations_each_way = int(
-                        ceil(num_rotations_per_image / 2)) + 1
+                    rotations_each_way = (int(
+                        ceil(num_rotations_per_image / 2)) + 2)
 
                     image_path = os.path.join(tag_path, file)
-                    # with open(image_path, "rb") as image_contents:
-                    #    print image_path
 
                     for number in range(rotations_each_way):
                         pil_image = Image.open(image_path)
                         rotated = pil_image.rotate(number)
 
+                        img_byte_arr = io.BytesIO()
+                        rotated.save(img_byte_arr, format='PNG')
+                        img_byte_arr = img_byte_arr.getvalue()
+
                         image_list.append(ImageFileCreateEntry(
-                            name=file, contents=rotated, tag_ids=[tags[seal_name].id]))
+                            name=file, contents=img_byte_arr, tag_ids=[tags[seal_name].id]))
 
                         # batch the requests
                         if len(image_list) == 60:
-                            #send_images(trainer, project, image_list)
+                            send_images(trainer, project, image_list)
+
+                            image_list = []
+
+                    for cc_number in range(359-rotations_each_way, 360):
+                        pil_image = Image.open(image_path)
+                        rotated = pil_image.rotate(cc_number)
+
+                        img_byte_arr = io.BytesIO()
+                        rotated.save(img_byte_arr, format='PNG')
+                        img_byte_arr = img_byte_arr.getvalue()
+
+                        image_list.append(ImageFileCreateEntry(
+                            name=file, contents=img_byte_arr, tag_ids=[tags[seal_name].id]))
+
+                        # batch the requests
+                        if len(image_list) == 60:
+                            send_images(trainer, project, image_list)
 
                             image_list = []
 
     # send last images not hitting the 60 limit
-    #send_images(trainer, project, image_list)
+    send_images(trainer, project, image_list)
 
 
 def send_images(trainer, project, image_list):
